754eb86df020f45ec635d314164068ab04f38d3a
deannominator <thedeannominator@proton.me>
2025-07-04 13:25:42 -0400

    Update desktop.sh

--- install/desktop.sh
+++ install/desktop.sh
@@ -1,8 +1,6 @@
 yay -S --noconfirm --needed \
   brightnessctl playerctl pamixer pavucontrol wireplumber \
-  fcitx5 fcitx5-gtk fcitx5-qt fcitx5-configtool \
   wl-clip-persist clipse \
   nautilus sushi gnome-calculator \
-  1password-beta 1password-cli \
-  chromium vlc \
+  chromium vlc mpv ghostty \
   evince imv
